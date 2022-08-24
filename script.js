66fcaa73b2981967882d9edbba3b3b8e3e04e239
vignesh2718 <vigneshn2718@gmail.com>
2022-08-24 20:59:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const counter1 = document.querySelector('.bell-notification');
+const counter2 = document.querySelector('.msg-notification');
+
+counter1.setAttribute('current-count',16);
+counter2.setAttribute('current-count',27);
